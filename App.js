88234b2a4e137674fdab061cd70055ad1965d070
angel <fsguoangel@hotmail.com>
2022-05-30 20:31:23 +0800

    Setting Up the Theme

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant-screen.js";
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
